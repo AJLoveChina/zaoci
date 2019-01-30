13111a62a7c90d2cb318903a8ad93f6a69316d4d
Baker.He <Baker.He@zoom.us>
2019-01-30 11:33:26 +0800

    init

---
+++ components/box/box.js
@@ -0,0 +1,23 @@
+// components/box/box.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+      word: String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
